fa3d4596df1247370852e1d3702818b38e25a242
Daniel Viloria <daniel115valdez@gmail.com>
2025-05-05 22:51:54 -0500

    Agregue carpetas

--- database/confbd.py
+++ database/confbd.py
@@ -6,3 +6,5 @@ def inicializar(app):
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://gpbus_user:EVMzvGQ3u8jpr41bZ6BJpRDguftJzdvP@dpg-d07ptj95pdvs73ad24gg-a.oregon-postgres.render.com/hospital'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)  
+
+
